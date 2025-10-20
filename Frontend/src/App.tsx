a62a51c2298752c85152add0f87018c4458907ab
akshithkonda1 <52667856+akshithkonda1@users.noreply.github.com>
2025-10-20 16:59:43 -0500

    Revert "feat: add Nexus layout shell with system settings menu"

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,15 +1,7 @@
-import { Outlet } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import React from "react";
+import ConsumerChat from "./consumer/ConsumerChat";
+import "./styles/consumer.css";
 
 export default function App() {
-  return (
-    <div className="min-h-screen flex flex-col bg-black text-white">
-      <Header />
-      <main className="flex-1 mx-auto w-full max-w-7xl px-4 py-6">
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
-  );
+  return <ConsumerChat />;
 }
