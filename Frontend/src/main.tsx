b2f36f5b5fdeba8f8a1eae901d331b249bedc967
akshithkonda1 <52667856+akshithkonda1@users.noreply.github.com>
2025-11-03 19:18:10 -0600

    Fix rendering syntax in main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,15 +1,16 @@
-
-import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "@/App";
 import { ThemeProvider } from "@/shared/ui/theme/ThemeProvider";
 import { registerServiceWorker } from "@/serviceWorker";
 import "@/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render( 
-  <React.StrictMode> 
-    <ThemeProvider> 
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>, );
+  </React.StrictMode>,
+);
+
 registerServiceWorker();
