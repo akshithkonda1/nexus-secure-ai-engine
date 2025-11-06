c809a6f481b549473214afd5c196539e976d38f8
akshithkonda1 <52667856+akshithkonda1@users.noreply.github.com>
2025-11-06 16:50:52 -0600

    Update main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,14 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "./theme/useTheme";
 import "./styles/globals.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
