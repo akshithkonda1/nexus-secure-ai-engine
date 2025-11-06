ec2d0eb63f9f1f11e8ad2258cce7e40b20a360fb
akshithkonda1 <52667856+akshithkonda1@users.noreply.github.com>
2025-11-06 03:09:28 -0600

    Update main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-import "./styles/globals.css";
 import { ThemeProvider } from "./theme/useTheme";
+import "./styles/globals.css";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root element #root not found");
