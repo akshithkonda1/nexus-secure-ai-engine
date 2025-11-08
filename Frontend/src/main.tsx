cb1886aaa6e456d844052f383f3d3a4b48f054cf
akshithkonda1 <52667856+akshithkonda1@users.noreply.github.com>
2025-11-07 18:23:20 -0600

    Update main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "@/theme/useTheme";
-import { AppRouter } from "@/router/AppRouter";
-import "@/styles/globals.css";
+import App from "./App";
+import { ThemeProvider } from "./theme/useTheme";
+import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
