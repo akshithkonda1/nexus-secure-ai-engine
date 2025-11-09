7ce836603494b4f4418a158f2838d39c449c842e
akshithkonda1 <52667856+akshithkonda1@users.noreply.github.com>
2025-11-09 04:17:42 -0600

    Update main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,18 +1,15 @@
+// Frontend/src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { ThemeProvider } from "./theme/useTheme";
 
-import "./styles/globals.css";
-import "./styles/theme-nexus.css";
+import "@/styles/globals.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
