44ac3b29a4c4329ce4a8e57939c90f0a3478175b
akshithkonda1 <52667856+akshithkonda1@users.noreply.github.com>
2025-11-09 04:19:11 -0600

    Update main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import "@/styles/globals.css";
+import "./styles/globals.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
