59baac38208ad39cbe9118b5fb620277758c1d6a
akshithkonda1 <52667856+akshithkonda1@users.noreply.github.com>
2025-12-09 23:54:45 -0600

    Implement Ryuzen Toron v2.5H+ pipeline

--- ryuzen/engine/__init__.py
+++ ryuzen/engine/__init__.py
@@ -3,5 +3,14 @@ from .toron_engine import ToronEngine, run_sync
 from .debate_engine import DebateEngine
 from .validation import ValidationEngine
 from .cache import InMemoryCache
+from .toron_v25hplus import RyuzenToronV25HPlus, ModelAbstractionLayer
 
-__all__ = ["ToronEngine", "run_sync", "DebateEngine", "ValidationEngine", "InMemoryCache"]
+__all__ = [
+    "ToronEngine",
+    "run_sync",
+    "DebateEngine",
+    "ValidationEngine",
+    "InMemoryCache",
+    "RyuzenToronV25HPlus",
+    "ModelAbstractionLayer",
+]
