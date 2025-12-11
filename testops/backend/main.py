66f1d5d151ed7a84deb326539f9d7a8d63327264
akshithkonda1 <52667856+akshithkonda1@users.noreply.github.com>
2025-12-11 16:31:28 -0600

    Fix TestOps import packages

--- testops/backend/main.py
+++ testops/backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers.testops_router import router as testops_router
+from testops.backend.routers.testops_router import router as testops_router
 
 app = FastAPI(title="Ryuzen TestOps Backend")
 
