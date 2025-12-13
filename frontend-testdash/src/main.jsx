989bbc70b16a782ce68f2d9eb0167bf912716291
akshithkonda1 <52667856+akshithkonda1@users.noreply.github.com>
2025-12-13 03:37:49 -0600

    Remove all frontend code

--- frontend-testdash/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import TestDashboard from './pages/TestDashboard.jsx';
-import './styles.css';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<TestDashboard />);
--- testops_frontend/src/components/TestRunButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function TestRunButton({ disabled, onClick }) {
-  return (
-    <button className="primary" disabled={disabled} onClick={onClick}>
-      RUN FULL TEST SUITE
-    </button>
-  )
-}
