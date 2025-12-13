2e533eab7c8653685b3e49086c72f4b812ec502f
akshithkonda1 <52667856+akshithkonda1@users.noreply.github.com>
2025-12-13 16:27:07 -0600

    Stabilize frontend Vite routing baseline

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,17 +1,5 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import AppShell from "./layouts/AppShell";
-import { ThemeProvider } from "./state/theme";
-import "./styles/globals.css";
 
-const App: React.FC = () => {
-  return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <AppShell />
-      </BrowserRouter>
-    </ThemeProvider>
-  );
-};
-
-export default App;
+export default function App() {
+  return <AppShell />;
+}
